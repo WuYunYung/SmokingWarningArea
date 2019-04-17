2d8a7df6ab59d919614f2e869524d71a0e89869d
Wilsonwu <justhurr@outlook.com>
2019-04-18 00:53:22 +0800

    更改了主页的新样式

--- home/views.py
+++ home/views.py
@@ -7,10 +7,10 @@ import os
 # Create your views here.
 def homepage(request):
     now = datetime.now()
-    filePath = '.\static\images'
+    filePath = '.\static\images\imageBox'
     filePathList = os.listdir(filePath)
     listNum = len(filePathList)
     List = map(str, range(listNum))  # 一个长度为100的 List
-    template = get_template('home/index.html')
+    template = get_template('home/homepage.html')
     html = template.render(locals())
     return HttpResponse(html)
